51b01429d904810dc74cf2d7357dfdad4cf47fa3
hongkang.feng <hongkang.feng@t2mobile.com>
2020-06-01 19:57:15 +0800

    add build script for android

---
+++ build_ffmpeg_shared_backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+NDK=/home/sdduser/Android/android-ndk-r8e
+SYSROOT=$NDK/platforms/android-14/arch-arm/
+TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86_64
+function build_one
+{
+	./configure \
+	--prefix=$PREFIX \
+	--enable-shared \
+	--disable-static \
+	--extra-cflags="-I$SYSROOT/usr/include/lame" \
+	--extra-ldflags="-L$SYSROOT/usr/lib" \
+	--disable-doc \
+	--disable-ffmpeg \
+	--disable-ffplay \
+	--disable-ffprobe \
+	--disable-ffserver \
+	--enable-filter=amix \
+	--enable-filter=scale \
+	--enable-decoder=mp3 \
+	--enable-libmp3lame \
+	--cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+	--target-os=linux \
+	--arch=arm \
+	--enable-cross-compile \
+	--sysroot=$SYSROOT \
+	$ADDITIONAL_CONFIGURE_FLAG
+	make clean
+	make
+	make install
+
+}
+CPU=arm
+PREFIX=$(pwd)/android/$CPU 
+ADDI_CFLAGS="-marm"
+build_one
